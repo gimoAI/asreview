47200fb931da76746159740d48402fb4f91fb476
Gijs Mourits <gijsmourits1998@hotmail.com>
2022-11-08 11:35:42 +0100

    custom transformer
    add Big Bird

--- asreview/models/feature_extraction/__init__.py
+++ asreview/models/feature_extraction/__init__.py
@@ -20,6 +20,7 @@ from asreview.models.feature_extraction.tfidf import Tfidf
 from asreview.models.feature_extraction.utils import get_feature_class
 from asreview.models.feature_extraction.utils import get_feature_model
 from asreview.models.feature_extraction.utils import list_feature_extraction
+from asreview.models.feature_extraction.ctrans import TRANSF
 
 """Feature extraction converts texts into features.
 
